9ae1c1bc8c26bab4b88bc6eadc9d0105ef86a0b3
Dnyaneshwar Tupe <116621055+Dnyanesh3256@users.noreply.github.com>
2024-11-15 11:03:43 +0530

    Add files via upload

---
+++ Loops/Q5.java
@@ -0,0 +1,14 @@
+/*
+What is wrong in the following program
+*/
+
+public class Q5 {
+    public static void main(String[] args) {
+        int i;
+        for(i=0;i<=5;i++){
+            System.out.println("i = "+i);
+        }
+
+        System.out.println("i after the loop : "+i);
+    }
+}
