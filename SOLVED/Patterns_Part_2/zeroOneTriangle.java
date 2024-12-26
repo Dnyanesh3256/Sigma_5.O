fa6567ea290d1a01ab7fca8aa6e1a389666f7a40
Dnyaneshwar Tupe <116621055+Dnyanesh3256@users.noreply.github.com>
2024-12-26 21:28:56 +0530

    Add files via upload

---
+++ SOLVED/Patterns_Part_2/zeroOneTriangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class zeroOneTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = sc.nextInt();
+        int x = 1;
+        int y = 0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if(((i+j)%2) == 0){
+                    System.out.print(x + " ");
+                }else{
+                    System.out.print(y + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
